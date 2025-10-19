cb2b494b0bbd0acae4af35630b32d335646ec7c4
miguel <miguel3497@outlook.com>
2025-10-19 16:26:45 -0600

    Funciona pero no recorre con cero en la parte entera

--- automaton.cpp
+++ automaton.cpp
@@ -1,29 +1,257 @@
 #include <iostream>
 
-int wich_case(char c){
+int wich_case(char c);
+int is_correct(char *str);
+int int_len(char *str);
+int mantisa_len(char *str);
+int char_index(char c, char *str);
+void get_pow(char *str, char *pow_arr);
+int char_to_int(char *pow_arr, int arr_len);
+int n_digits(int num);
+void int_to_char(int num, char *pow_arr, int arr_len);
+void corr_pow(char *str, int n_spaces, char *pow_arr, int arr_len);
+void correct_expr(char *str);
 
-  if (c >= 48 && c <= 57) {
-    //std::cout << c << ": Es numero" << '\n';
-    return 1;
-  }else if(c == 46){
-    //std::cout << c << ": Es punto" << '\n';
-    return 2;
-  }else if (c == 69 || c == 101){
-    //std::cout << c << ": Es una E o e" << '\n';
-    return 3;
-  }else if (c == 43 || c == 45){
-    //std::cout << c << ": Es un signo" << '\n';
-    return 4;
+int main(int argc, char *argv[])
+{
+  int correct;
+  
+  correct = is_correct(argv[1]);
+  
+  if (!correct){
+    correct_expr(argv[1]);
   }
-
-  return 0;  
+  
+  return 0;
 }
 
-int main(int argc, char *argv[]){
-  int s,c,mantisa,i;
-  char *str;
+int int_len(char *str){
+  int len,i;
 
-  str = argv[1];
+  len = 0;
+  i = 0;
+  while (str[i] != '.') {
+    len++;
+    i++;
+  }
+
+  if(wich_case(str[0]) == 4){
+    len--;
+  }
+
+  return len;
+}
+
+int mantisa_len(char *str){
+  int len,i;
+
+  len = 0;
+  i = 0;
+  while (str[i] != '.') i++;
+  
+  while (str[i] != '\0') {
+    if (str[i] == 69 || str[i] == 101) break;
+    len++;
+    i++;
+  }
+
+  return len-1;
+}
+
+void get_pow(char *str, char *pow_arr){
+  int i,j;
+
+  i = 0;
+  while (str[i] != '\0') {
+    if (str[i] == 'e' || str[i] == 'E') {
+      i++;
+      break;
+    }
+    i++;
+  }
+  
+  j = 0;
+  while (str[i] != '\0') {
+    pow_arr[j++] = str[i++];
+  }
+
+  pow_arr[j] = '\0';
+}
+
+int char_to_int(char *pow_arr, int arr_len){
+  int num,base,temp_num,i;
+
+  base = 1;
+  num = 0;
+  for (i = arr_len-1; i >= 0; i--) {
+    if (wich_case(pow_arr[i]) == 1) {
+      temp_num = pow_arr[i] - 48;
+      num += (base * temp_num);
+      base *= 10;
+    }
+  }
+
+  if (pow_arr[0] == '-'){
+    num *= -1;
+  }
+
+  return num;
+}
+
+int n_digits(int num){
+  int n_digits, i;
+
+  if (num < 0) {
+    num *= -1;
+  }
+
+  n_digits = 0;
+  while (num > 0) {
+    for (i = 0; i < 10; ++i) {
+      if ((num-i)%10==0) {
+        num -= i;
+        num /= 10;
+        break;
+      }
+    }
+    n_digits++;
+  }
+
+  return n_digits;
+}
+
+void int_to_char(int num, char *pow_arr, int arr_len){
+  int n_digs, start, i, j;
+  
+  n_digs = n_digits(num);
+
+  if (num < 0){
+    num *= -1;
+    pow_arr[0] = '-';
+  }else {
+    pow_arr[0] = '0';
+  }
+  
+  j = 0;
+  while (num != 0) {
+    for (i = 0; i < 10; ++i) {
+      if ((num-i)%10 == 0) {
+        pow_arr[n_digs-j] = i+48;
+        num -= i;
+        num /= 10;
+        j++;
+        break;
+      }
+    }
+  }
+
+  if (n_digs + 1 < arr_len) {
+    pow_arr[n_digs+1] = '\0';
+  }else {
+    printf("Espacio insuficiente\n");
+  }
+}
+
+void corr_pow(char *str, int n_spaces, char *pow_arr, int arr_len){
+  int pot, e_ind, i, j;
+
+  pot = char_to_int(pow_arr, arr_len);
+  pot += n_spaces;
+  
+  int_to_char(pot, pow_arr, arr_len);
+
+  e_ind = char_index('e', str);
+  if (!e_ind){
+    e_ind = char_index('E', str);
+  }
+  e_ind++;
+
+  j = 0;
+  for (i = 0; i < arr_len; ++i) {
+    if (pow_arr[i] == '0') {
+      continue;
+    }
+    str[j+e_ind] = pow_arr[i];
+    j++;
+  }
+}
+
+int char_index(char c, char *str){
+  int i;
+
+  i = 0;
+  while (str[i] != '\0'){
+    if (str[i] == c){
+      return i;
+    }
+    i++;
+  }
+ 
+  return 0;
+}
+
+void correct_expr(char *str){
+  //char sig,d1,d2,d3,m1,m2;
+  int  e_ind, dot_ind, intg, mant, pow_len, i, j, k;
+  char digs[4];
+  char *expr = str;
+  char pow_arr[pow_len];
+
+  pow_len = 6;
+  intg = int_len(str);
+  mant = mantisa_len(str);
+  i = 0;
+  j = 0;
+  k = 4;
+
+  if (wich_case(str[i]) != 4) {
+    digs[i++] = '0';
+    k = 3;
+  }
+
+  for (j = 0; j < k; ++j) {
+    if (wich_case(str[j]) == 2){
+      k++;
+      continue;
+    }
+    digs[i++] = str[j];
+  }
+
+  if (digs[0] != '0'){
+    expr[0] = digs[0];
+    expr[1] = digs[1];
+    expr[2] = '.';
+    expr[3] = digs[2];
+    expr[4] = digs[3];
+    j = 5;
+  }else {
+    expr[0] = digs[1];
+    expr[1] = '.';
+    expr[2] = digs[2];
+    expr[3] = digs[3];
+    j = 4;
+  }
+
+  e_ind = char_index('e', str);
+  if (!e_ind){
+    e_ind = char_index('E', str);
+  }
+
+  i = e_ind;
+  while (str[i] != '\0') {
+    expr[j++] = str[i++];
+  }
+
+  expr[j] = '\0';
+  
+  get_pow(expr, pow_arr);
+  corr_pow(expr, intg-1, pow_arr, pow_len); 
+
+  std::cout << "Corrección: "<< expr << '\n';
+}
+
+int is_correct(char *str){
+  int s,c,mantisa,i;
 
   s = 0;
   i = 0;
@@ -31,7 +259,6 @@ int main(int argc, char *argv[]){
 
   while (str[i] != '\0') {    
     c = wich_case(str[i]);
-    printf("s:%d c:%d mantisa:%d\n", s, c, mantisa);
     switch (s) {
       case 0:
         if (c == 1){
@@ -105,15 +332,26 @@ int main(int argc, char *argv[]){
     i++;
   }
 
-  if (s == 2){
-    printf("Es entero\n"); 
-  }else if (s == 4){
-    printf("Es un flotante\n");
-  }else if (s == 7){
-    printf("Es una notación científica\n");
-  }else {
-    printf("No válido\n");
+  if (s == 7){
+    std::cout << "Es notación científica" << '\n';
+    return 1;
+  } 
+
+  std::cout << "No válido" << '\n';
+  return 0;
+}
+
+int wich_case(char c){
+
+  if (c >= 48 && c <= 57) {
+    return 1;
+  }else if(c == 46){
+    return 2;
+  }else if (c == 69 || c == 101){
+    return 3;
+  }else if (c == 43 || c == 45){
+    return 4;
   }
 
-  return 0;
+  return 0;  
 }
